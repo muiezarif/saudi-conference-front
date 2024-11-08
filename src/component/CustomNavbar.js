a1e651fc4530c4a48ad00501dd6ef3e04254da4a
zaman <zamantariq247@gmail.com>
2024-11-09 02:48:56 +0500

    responsive

--- src/component/CustomNavbar.js
+++ src/component/CustomNavbar.js
@@ -29,7 +29,7 @@ export default function CustomNavbar() {
         }
     }
     return (
-        <div style={{marginBottom: '15rem', fontFamily: "DiodrumRegular"}}>
+        <div style={{marginBottom: '8rem', fontFamily: "DiodrumRegular"}}>
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8 custom-navbar-bg">
                     <div className="flex lg:flex-1">
