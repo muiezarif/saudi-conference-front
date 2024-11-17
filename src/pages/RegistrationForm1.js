0fac6a5bd0c34b8570fb1994d344df8f1092f618
muiezarif <36100308+muiezarif@users.noreply.github.com>
2024-11-17 14:34:05 +0500

    form test sheet

--- src/pages/RegistrationForm1.js
+++ src/pages/RegistrationForm1.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import {Col, Container, Form, FormGroup, Input, Label, Row} from 'reactstrap';
-import {Base} from "../component/Base";
+import React, { useState } from 'react';
+import { Col, Container, Form, FormGroup, Input, Label, Row, Button } from 'reactstrap';
+import { Base } from "../component/Base";
+import axios from 'axios';
 
 const RegistrationForm1 = () => {
     const style = {
@@ -23,54 +24,104 @@ const RegistrationForm1 = () => {
             marginBottom: "1rem",
             color: "#606060"
         }
-    }
+    };
+
+    // State to manage form data
+    const [formData, setFormData] = useState({
+        firstName: '',
+        familyName: '',
+        email: '',
+        phone: '',
+        jobTitle: '',
+        organization: '',
+        membershipNumber: '',
+        membershipType: ''
+    });
+
+    // Handle input change
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(
+                'https://script.google.com/macros/s/AKfycbydJuQSXWxv3Op-fVN0glvvyOxH0Je5OfpfQtiTgFG5SlODaZilSiQy9ABDd5CjF2RsVw/exec',
+                formData
+            );
+            alert('Form submitted successfully!');
+        } catch (error) {
+            console.error('Error submitting form:', error);
+            alert('Failed to submit the form.');
+        }
+    };
+
     return (
         <Base>
-            <Container style={{marginTop: "13rem",}}>
+            <Container style={{ marginTop: "13rem" }}>
                 <Row>
                     <Col className="text-center">
-                        <h2 style={{color: '#0b1a8a', textAlign: 'right', fontFamily: "DiodrumBold"}}>فئات التذاكر</h2>
-                        <h3 style={{color: '#009688', textAlign: 'right', fontFamily: "DiodrumBold"}}>:مزايا فئة
-                            VIP</h3>
+                        <h2 style={{ color: '#0b1a8a', textAlign: 'right', fontFamily: "DiodrumBold" }}>فئات التذاكر</h2>
+                        <h3 style={{ color: '#009688', textAlign: 'right', fontFamily: "DiodrumBold" }}>
+                            :مزايا فئة VIP
+                        </h3>
                     </Col>
                 </Row>
-                <Row className='mt-2' style={{textAlign: "right", fontFamily: "DiodrumRegular"}}>
-                    <Col style={{gap: '12px'}}>
-                        <div className="d-inline-flex" style={{paddingRight: '100px'}}>
-                            <p style={style.para}>ضيافة فاخرة</p>
-                            <div style={style.dot}/>
+                <Row className='mt-2' style={{ textAlign: "right", fontFamily: "DiodrumRegular" }}>
+                    <Col style={{ gap: '12px' }}>
+                        <div className="d-inline-flex" style={{ paddingRight: '100px' }}>
+                            <p>ضيافة فاخرة</p>
+                            <div style={style.dot} />
                         </div>
-                        <div className="d-inline-flex" style={{paddingRight: '100px'}}>
-                            <p style={style.para}>خدمة صف السيارات</p>
-                            <div style={style.dot}/>
+                        <div className="d-inline-flex" style={{ paddingRight: '100px' }}>
+                            <p>خدمة صف السيارات</p>
+                            <div style={style.dot} />
                         </div>
                         <div className="d-inline-flex">
-                            <p style={style.para}>حضور حفل الافتتاح </p>
-                            <div style={style.dot}/>
+                            <p>حضور حفل الافتتاح</p>
+                            <div style={style.dot} />
                         </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="text-center m-2">
-                        <h4 className="" style={{color: '#606060', textAlign: 'right', fontFamily: "DiodrumBold"}}>بيانات
-                            التسجيل
-                            الرئيسية</h4>
+                        <h4 style={{ color: '#606060', textAlign: 'right', fontFamily: "DiodrumBold" }}>
+                            بيانات التسجيل الرئيسية
+                        </h4>
                     </Col>
                 </Row>
 
-                <Form style={{textAlign: 'right', fontFamily: "DiodrumRegular"}}>
+                <Form onSubmit={handleSubmit} style={{ textAlign: 'right', fontFamily: "DiodrumRegular" }}>
                     <Row form>
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="firstName" style={style.labelStyle}>الاسم الأول</Label>
-                                <Input type="text" name="firstName" id="firstName" placeholder=""
-                                       style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="firstName"
+                                    id="firstName"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
-                        <Col md={6} style={{textAlign: 'right'}}>
+                        <Col md={6}>
                             <FormGroup>
                                 <Label for="email" style={style.labelStyle}>البريد الإلكتروني</Label>
-                                <Input type="email" name="email" id="email" placeholder="" style={style.textField}/>
+                                <Input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                     </Row>
@@ -79,14 +130,27 @@ const RegistrationForm1 = () => {
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="phone" style={style.labelStyle}>رقم الجوال</Label>
-                                <Input type="text" name="phone" id="phone" placeholder="" style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="phone"
+                                    id="phone"
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="jobTitle" style={style.labelStyle}>المسمى الوظيفي</Label>
-                                <Input type="text" name="jobTitle" id="jobTitle" placeholder=""
-                                       style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="jobTitle"
+                                    id="jobTitle"
+                                    value={formData.jobTitle}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                     </Row>
@@ -95,15 +159,27 @@ const RegistrationForm1 = () => {
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="organization" style={style.labelStyle}>اسم الجهة</Label>
-                                <Input type="text" name="organization" id="organization" placeholder=""
-                                       style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="organization"
+                                    id="organization"
+                                    value={formData.organization}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="membershipNumber" style={style.labelStyle}>رقم العضوية</Label>
-                                <Input type="text" name="membershipNumber" id="membershipNumber" placeholder=""
-                                       style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="membershipNumber"
+                                    id="membershipNumber"
+                                    value={formData.membershipNumber}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                     </Row>
@@ -112,18 +188,34 @@ const RegistrationForm1 = () => {
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="familyName" style={style.labelStyle}>اسم العائلة</Label>
-                                <Input type="text" name="familyName" id="familyName" placeholder=""
-                                       style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="familyName"
+                                    id="familyName"
+                                    value={formData.familyName}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="membershipType" style={style.labelStyle}>نوع العضوية</Label>
-                                <Input type="text" name="membershipNumber" id="membershipNumber" placeholder=""
-                                       style={style.textField}/>
+                                <Input
+                                    type="text"
+                                    name="membershipType"
+                                    id="membershipType"
+                                    value={formData.membershipType}
+                                    onChange={handleChange}
+                                    style={style.textField}
+                                />
                             </FormGroup>
                         </Col>
                     </Row>
+
+                    <Button type="submit" color="primary" style={{ marginTop: '20px' }}>
+                        Submit
+                    </Button>
                 </Form>
             </Container>
         </Base>
